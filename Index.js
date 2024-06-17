e5e2605c8e66e9221b3e9d849a149b448a724693
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-06-17 12:31:46 -0700

    Starting node

---
+++ Index.js
@@ -0,0 +1,27 @@
+const fs=require('fs')
+//  fs.readFile('a.txt','utf-8',(err,data)=>{
+//     console.log(data)
+//  })
+
+// fs.writeFile('b.txt', "hi Kinshuk", (err) => {
+//     if (err) {
+//       console.error("Error writing to file:", err);
+//     } else {
+//       console.log("written to file");
+//     }
+//   });
+ fs.open('abc.js','a',function(err,file){
+    console.log('saved')
+ })
+ fs.appendFile('abc.js',"hi kinshuk",function(err){
+    if(err)throw err;
+    console.log('updated file data')
+ })
+ fs.rename('new.html','excellencehtml',function(err){
+    if(err)throwerr;
+    console.log("mission complete")
+ })
+//  fs.unlink("excellence.html",function(err){
+//     if(err)throw err;
+//     console.log("file deleted")
+//  })
---
+++ abc.js
@@ -0,0 +1,1 @@
+hi kinshukhi kinshukhi kinshuk
