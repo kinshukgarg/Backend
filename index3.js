f57a07f569468eea2b0431a76bd00c7d086d0968
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-06-25 22:47:55 -0700

    updates last code

--- index3.js
+++ index3.js
@@ -5,7 +5,7 @@ var app=express();
 app.listen(5004,function(){
     console.log("server started")
 })
-app.get("/home",function(req,resp){
+app.get("/",function(req,resp){
     resp.write("welcome")
     resp.end()
 })
---
+++ index7.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.send("hello kinshuk");
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname,'Home.html'));
+});
+
+app.get('/api', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fetchapi.html'));
+});
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+});
