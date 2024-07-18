70a16eb141701872fba0b674462abf118d33d245
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-18 00:56:32 -0700

    updated code and make correct connection in mongoose

--- Mongoose/Mongoose.js
+++ Mongoose/Mongoose.js
@@ -1,14 +1,6 @@
-const {MongoClient}=require('mongodb')
 
-const url='mongodb+srv://cloudmongodb12:AU51TlGJSTUQnD8h@cluster0.no9cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoose= require('mongoose');
 
-const database="ecommerce"
-
-const client=new MongoClient(url)
-
-async function dbConnect(){
-    let result=await  client.connect()
-    db=result.db(database)
-    return db.collection('users')
-}
+const dbConnect=async ()=>{
+    await mongoose.connect('mongodb+srv://cloudmongodb12:AU51TlGJSTUQnD8h@cluster0.no9cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')}
 module.exports=dbConnect;
--- Mongoose/index2.js
+++ Mongoose/index2.js
@@ -1,39 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dbConnect = require('./Mongoose');
+const UsersModel = require('./UsersSchema');
 
-const express=require('express')
-const app=express()
+app.use(bodyParser.json());
 
-const mongodb=require('mongodb')
-const mongoose=require('mongoose')
+dbConnect();
 
-const bodyParser=require('body-parser')
-const dbConnect=require('./Mongoose')
-const Userssche=require('./UsersSchema')
+app.get('/list', async (req, resp) => {
+    const data = await UsersModel.find();
+    resp.send(data);
+});
 
+app.post('/data', async (req, resp) => {
+    const user = new UsersModel(req.body);
+    const result = await user.save();
+    resp.send(result);
+});
 
-app.get('/list', async (req,resp) =>{
-let data= await dbConnect()
-let result=data.find().toArray()
-console.log(data)
-resp.send(data)
-})
-
-app.post('/data',async(req,resp)=>{
-    let data =await dbConnect()
-    let result =await data.insertone(req.body)
-    resp.send(result)
-})
- app.delete('/user/:id',async (req,resp)=>{
-    console.log(req.params.id);
-    const data = await dbConnect();
+app.delete('/user/:id', async (req, resp) => {
     const result = await UsersModel.deleteOne({ _id: req.params.id });
-    resp.send('compeletd')
- })
+    resp.send(result);
+});
 
+app.put('/update/:id', async (req, resp) => {
+    const result = await UsersModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    resp.send(result);
+});
 
-
-
-
-
-app.listen(7500,()=>{
-    console.log('server started at 7500')
-})
+app.listen(7500, () => {
+    console.log('server started at 7500');
+});
